b914ab7964c16f219335e96e30d42ff70c01f69b
Mariana Medeiros <marianas.medeiros@yahoo.com.br>
2018-01-20 00:58:40 -0200

    Trabalhando com Streams - Aplicação básica

--- ManipulacaoDeArquivo/Form1.Designer.cs
+++ ManipulacaoDeArquivo/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.caixaDeTexto = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // caixaDeTexto
+            // 
+            this.caixaDeTexto.Location = new System.Drawing.Point(12, 12);
+            this.caixaDeTexto.Multiline = true;
+            this.caixaDeTexto.Name = "caixaDeTexto";
+            this.caixaDeTexto.Size = new System.Drawing.Size(260, 169);
+            this.caixaDeTexto.TabIndex = 0;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(197, 196);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 1;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.caixaDeTexto);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox caixaDeTexto;
+        private System.Windows.Forms.Button btnSalvar;
     }
 }
 
--- ManipulacaoDeArquivo/Form1.cs
+++ ManipulacaoDeArquivo/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,40 @@ namespace ManipulacaoDeArquivo
 {
     public partial class Form1 : Form
     {
+        private string pathArquivo;
+
         public Form1()
         {
             InitializeComponent();
+            this.pathArquivo = "arquivo01.txt";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(pathArquivo)){
+                Stream entrada = File.Open(pathArquivo, FileMode.Open);
+                StreamReader sr = new StreamReader(entrada);
+                string linha = sr.ReadLine();
+
+                while (linha != null)
+                {
+                    caixaDeTexto.Text += linha;
+                    linha = sr.ReadLine();
+                }
+
+                sr.Close();
+                entrada.Close();
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Stream saida = File.Open(pathArquivo, FileMode.Create);
+            StreamWriter sw = new StreamWriter(saida);
+            sw.Write(caixaDeTexto.Text);
+
+            sw.Close();
+            saida.Close();
         }
     }
 }
