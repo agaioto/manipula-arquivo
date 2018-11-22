69ab190acd4e3c0b59e890dc45ddbdf44c7d6dd5
agaioto <angelo.r.gaioto@gmail.com>
2018-11-22 18:31:16 -0200

    Teste

--- ManipulacaoDeArquivo/Program.cs
+++ ManipulacaoDeArquivo/Program.cs
@@ -14,6 +14,7 @@ namespace ManipulacaoDeArquivo
         [STAThread]
         static void Main()
         {
+            Console.WriteLine("teste");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
